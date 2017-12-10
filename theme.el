4fe1e1bfd705093fee1f412accfef10db5fb5119
Matt Dee <foggerty@gmail.com>
2017-12-10 18:34:33 +1300

    Added neo-tree and beacon.

--- theme.el
+++ theme.el
@@ -4,18 +4,38 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
-;;; Theme
+;; Spaceline theme (i.e. the mode-line)
+(use-package spaceline
+  :config
+  (require 'spaceline-config)
+  (setq powerline-default-separator 'utf-8)
+  (spaceline-emacs-theme))
+
+;; Beacon - show where the cursor is after scrolling.
+(use-package beacon
+  :config
+  (beacon-mode 1))
+
+;; A tree-view on the side of the frame
+(use-package neotree
+  :config
+  (global-set-key [f8] 'neotree-toggle)
+  (setq neo-theme 'ascii)
+  (setq neo-smart-open t)
+  (setq projectile-switch-project-action 'neotree-projectile-action))
+
+;; Theme
 (require 'color)
 
 (load-file "~/.emacs.d/old/tangotango2.el")
 
 (let ((bg (face-attribute 'default :background)))
-    (custom-set-faces
-     `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 10)))))
-     `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
-     `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 50)))))
-     `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
-     `(company-tooltip-common ((t (:inherit font-lock-constant-face))))))
+  (custom-set-faces
+   `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 10)))))
+   `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
+   `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 50)))))
+   `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
+   `(company-tooltip-common ((t (:inherit font-lock-constant-face))))))
 
 ;; Cursor
 (set-default 'cursor-type 'bar)
