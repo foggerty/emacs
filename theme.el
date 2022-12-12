f82fea1effe7291746858de39e20cdb3abf8fe25
Foggerty <foggerty@gmail.com>
2022-12-12 19:47:35 +1300

    Theme now works with stand-alone.

--- theme.el
+++ theme.el
@@ -1,22 +1,28 @@
+;;; Make pretty with the help of pywal.
+(custom-set-variables '(custom-safe-themes t))
+
+(use-package ewal-spacemacs-themes)
+
+
 ;;; This starts to get messy when you want this config to work with
 ;;; text, gui and daemon......
 (add-hook 'after-make-frame-functions
-	  (when (display-graphic-p)
-	  (helper-set-font '("Sauce Code Pro Nerd Font"))
-          (set-frame-parameter nil 'alpha-background 0.8)
-          (set-face-attribute 'default nil
-                              :height 120)))
+	       (progn
+            (toggle-menu-bar-mode-from-frame 0)
+            (toggle-tool-bar-mode-from-frame 0)
+            (load-theme 'ewal-spacemacs-classic))
+	       (when (display-graphic-p)
+	         (helper-set-font '("Sauce Code Pro Nerd Font"))
+            (set-frame-parameter nil 'alpha-background 0.8)
+            (set-face-attribute 'default nil
+                                :height 120)))
 
 
 ;;; Basic tidy ups
 (setq inhibit-startup-screen t)
-(setq tool-bar-mode nil)
-
 (add-to-list 'default-frame-alist
              '(vertical-scroll-bars . nil)
              '(menu-bar-lines . nil))
-
-(toggle-menu-bar-mode-from-frame 0)
 
 
 ;;; Beacon - show where the cursor is after scrolling.
@@ -25,16 +31,7 @@
   (beacon-mode 1))
 
 
-;;; Make pretty with the help of pywal.
-(custom-set-variables '(custom-safe-themes t))
-(use-package ewal-spacemacs-themes)
-(load-theme 'ewal-spacemacs-classic)
-
-
 ;;; Cursor
 (set-default 'blink-cursor-blinks 0) ; always blink
 (set-default 'blink-cursor-interval 0.25) ; blink faster!
 (set-default 'cursor-type 'bar)
-
-
-;;; Fonts/faces
