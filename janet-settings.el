cebfe1656da6e19df6791dabb55824d908c1f354
Foggerty <foggerty@gmail.com>
2023-04-25 00:56:27 +1200

    Twooking

--- janet-settings.el
+++ janet-settings.el
@@ -1,13 +1,4 @@
 (use-package janet-mode)
 
-(load-from-init  "inf-janet.el")
-
-;;  (setq inf-janet-program "~/janet/build/janet")
-;;    or
-;;  (setq inf-janet-program '("localhost" . 5555))
-
-(setq inf-janet-program (shell-command-to-string "which janet"))
-
-(add-hook 'janet-mode-hook #'inf-janet-minor-mode)
 (add-hook 'janet-mode-hook #'paredit-mode)
 (add-hook 'janet-mode-hook #'aggressive-indent-mode)
