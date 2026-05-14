c36be866545756a989473f5eb5e58f42a830271b
Foggerty <foggerty@gmail.com>
2026-05-14 18:04:36 +1200

    Finally figured out how to set fonts properly.

--- theme.el
+++ theme.el
@@ -9,7 +9,7 @@
 
 (menu-bar-mode 0)
 (tool-bar-mode 0)
-(scroll-bar-mode 0)
+;;(scroll-bar-mode 0)
 
 ;; (set-window-scroll-bars (minibuffer-window) 0 'none nil nil t)
 
@@ -44,11 +44,10 @@
              "/home/matt/.emacs.d/themes")
 
 (modify-all-frames-parameters
- '((font . "SauceCodePro Nerd Font Mono-13")
-   (alpha-background . 0.68)))
+ '((alpha-background . 0.68)))
 
 (use-package ewal-doom-themes)
-(setq ewal-doom-one-padded-modeline 1
+(setq ewal-doom-one-padded-modeline 4
       ewal-doom-one-brighter-modeline 1)
 (load-theme 'ewal-doom-one t)
 
@@ -63,3 +62,32 @@
         (set-face-background 'font-lock-comment-face "unspecified-bg" (selected-frame))))))
 
 (add-hook 'server-after-make-frame-hook #'on-frame-open)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Font customisation
+;;;
+;;
+
+(custom-theme-set-faces
+ 'user
+ '(variable-pitch ((t (:family "Sans" :width expanded :height 1.0 ))))
+ '(fixed-pitch ((t ( :family "Sauce Code Pro Nerd Font" :height 1.0 :inherit 'default))))
+ '(font-lock-comment-face ((t (:slant italic :weight semi-bold :background unspecified))))
+ '(show-paren-match ((t (:underline nil :foreground "red")))))
+
+(use-package org
+  :hook ((org-mode . variable-pitch-mode))
+  :config
+  (set-face-attribute 'org-block nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-table nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
+  (set-face-attribute 'org-level-1 :height 1.0)
+  (set-face-attribute 'org-level-2 :height 1.0)
+  (set-face-attribute 'org-block :inherit fixed-pitch)
+  (set-face-attribute 'org-block-begin-line :background unspecified)
+  (set-face-attribute 'org-block-end-line :background unspecified)
+  (set-face-attribute 'org-code :inherit (shadow fixed-pitch))
+  (set-face-attribute 'avy-lead-face :background unspecified)
+  (set-face-attribute 'avy-lead-face-0 :background unspecified))
